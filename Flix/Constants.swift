ac06f4fdda70f6c804156e94984b5e670a6eb1a2
Luis Rivera Rivera <luisrockero11@hotmail.com>
2022-09-08 12:38:23 -0400

    Added project files

---
+++ Flix/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Flix
+//
+//  Created by Luis Rivera Rivera on 9/6/22.
+//
+
+import Foundation
+
+struct Constants {
+    static let tableViewCell = "MovieCell"
+}
---
+++ Flix/MovieCell.swift
@@ -0,0 +1,26 @@
+//
+//  MovieCell.swift
+//  Flix
+//
+//  Created by Luis Rivera Rivera on 9/7/22.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
